a4bdfdc792073b0fab4ecc7c8bcba6aed6806b4b
mishanin <mishanin@starkovgrp.ru>
2023-11-13 10:59:34 +0500

    Крайние изменения Лехи (Перед переделыванием на StateView)

---
+++ Starkov.RoadMaps/Starkov.RoadMaps.Server/Task/TaskRouteHandlers.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Workflow;
+using Starkov.RoadMaps.Task;
+
+namespace Starkov.RoadMaps.Server
+{
+  partial class TaskRouteHandlers
+  {
+
+  }
+}
